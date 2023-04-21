b28d48a0f961378b82cc2ea327a82cb39ed81661
TetianaYavdoshchak <105789382+TetianaYavdoshchak@users.noreply.github.com>
2023-04-21 10:55:50 +0000

    Deploying to gh-pages from @ TetianaYavdoshchak/goit-js-hw-09@5fc9bee10b9425c2a29e8216010039d092d66e23 🚀

--- 01-color-switcher.b9813fd3.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;function n(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.btnStart.addEventListener("click",(function(r){t.body.style.background=n(),e=setInterval((()=>{t.body.style.background=n()}),1e3),t.btnStart.setAttribute("disabled","")})),t.btnStop.addEventListener("click",(function(n){t.btnStart.removeAttribute("disabled"),clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.b9813fd3.js.map
