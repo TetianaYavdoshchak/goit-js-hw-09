2757c30c61994d6e00359d63ee252f8c84c58ca4
TetianaYavdoshchak <tasik19091995@icloud.com>
2023-04-21 12:17:32 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+   }
 
+let intervalId = null;
+   
+refs.btnStart.addEventListener('click', startChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+function startChangeColor(event) {
+    refs.body.style.background = getRandomHexColor();
+    intervalId = setInterval(() => {
+        refs.body.style.background = getRandomHexColor()
+    }, 1000);
+    refs.btnStart.setAttribute('disabled', '');
+}
+
+function stopChangeColor(event) {
+    refs.btnStart.removeAttribute('disabled');
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
